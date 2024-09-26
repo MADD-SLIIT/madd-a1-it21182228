a9720dffc4d47d6b585390e4d04a9b7416e421c0
it21182228 <you@example.com>
2024-09-26 13:22:10 +0530

    commit 1

---
+++ app/src/main/java/com/example/mad/Activities/Home.kt
@@ -0,0 +1,12 @@
+package com.example.mad.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mad.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
